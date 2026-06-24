23743b7f22bd0ad682a5dad4561af78b2e065c9d
Muhammad Akbar Reishandy <akbar@reishandy.id>
2026-06-24 23:12:37 +0800

    Initial Commit

---
+++ YouriBeaconSimulator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YouriBeaconSimulator
+//
+//  Created by Muhammad Akbar Reishandy on 24/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouriBeaconSimulator/YouriBeaconSimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouriBeaconSimulatorApp.swift
+//  YouriBeaconSimulator
+//
+//  Created by Muhammad Akbar Reishandy on 24/06/26.
+//
+
+import SwiftUI
+
+@main
+struct YouriBeaconSimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
