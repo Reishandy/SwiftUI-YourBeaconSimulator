06605bbaed4c81b9c406364ef7b32d3a772e36e6
Muhammad Akbar Reishandy <akbar@reishandy.id>
2026-06-25 12:12:33 +0800

    broadcast item view

---
+++ YouriBeaconSimulator/Features/Broadcast/Components/MeasuredTXPowerView.swift
@@ -0,0 +1,18 @@
+//
+//  MeasuredTXPowerView.swift
+//  YouriBeaconSimulator
+//
+//  Created by Muhammad Akbar Reishandy on 25/06/26.
+//
+
+import SwiftUI
+
+struct MeasuredTXPowerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeasuredTXPowerView()
+}
