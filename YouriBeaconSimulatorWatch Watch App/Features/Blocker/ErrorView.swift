1c3430214e6dbd0e50f34fb6aa3cfbe672e9f5c7
Muhammad Akbar Reishandy <akbar@reishandy.id>
2026-07-10 12:08:49 +0800

    feat: error handling revamp

---
+++ YouriBeaconSimulatorWatch Watch App/Features/Blocker/ErrorView.swift
@@ -0,0 +1,18 @@
+//
+//  ErrorView.swift
+//  YouriBeaconSimulatorWatch Watch App
+//
+//  Created by Muhammad Akbar Reishandy on 10/07/26.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ErrorView()
+}
